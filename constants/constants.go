f3e85507f586fadc21a59e2605cc03a3c990c9c3
20-VIGNESH-K <vigneshvignesh7035@gmail.com>
2023-08-31 14:31:52 +0530

    added

---
+++ constants/constants.go
@@ -0,0 +1,5 @@
+package netxd_constants
+
+const ConnectionString = "mongodb+srv://vigneshk:1234@banking.obgwcv6.mongodb.net/?retryWrites=true&w=majority"
+const Port = ":3000"
+const DatabaseName = "banking"
